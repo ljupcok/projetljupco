a87c5bdb69c1a66b3b7d6a3ae8b217b2f84bdeef
Ljupco  Kukovski <kukovski.ljupco@gmail.com>
2020-04-05 15:56:32 +0200

    1er test

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<div id="bloc-page">
+		<header>
+			<?php include("menu.php")?>
+		</header>
+		<section class="titre-top">
+			<h1>Bienvenue sur notre site</h1>
+			
+		</section>
+
+		<section id="text-home">	
+		</section>
+
+		<section id="text-home2">	
+			  
+		</section>
+
+		<footer>
+			<?php include("pieddepage.php")?>
+		</footer>
+	</div>
+</body>
+</html>
